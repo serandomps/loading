f08affc2150ebd073c03e93c0a4a9022c4ead573
ruchiraw <ruchira.wageesha@gmail.com>
2014-07-21 00:10:30 +0530

    updating

---
+++ index.js
@@ -0,0 +1,41 @@
+var loaders = [];
+
+var find = function (sandbox) {
+    var i, o, loder,
+        length = loaders.length;
+    for (i = 0; i < length; i++) {
+        loder = loaders[i];
+        if (loder.sandbox === sandbox) {
+            o = {
+                index: i,
+                loader: loder
+            };
+            break;
+        }
+    }
+    return o;
+};
+
+var clean = function (sandbox) {
+    console.log('cleaning loader');
+    $('.loading', sandbox).remove();
+};
+
+module.exports = function (loader, renderer, sandbox, fn) {
+    var el = $('<div class="loading"><h2>loading</h2></div>');
+    sandbox.html(el);
+    loader(function (err, data) {
+        clean(sandbox);
+        renderer(data, function (err, fn) {
+            loaders.push({
+                sandbox: sandbox,
+                clean: fn
+            });
+        });
+    });
+    fn(false, function () {
+        var o = find(sandbox);
+        o.loader.clean ? o.loader.clean() : clean(sandbox);
+        //delete loaders[o.index];
+    });
+};
